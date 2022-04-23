1fe18d4bb13bb72030c64852ab5101a262eb06ae
David Malmström <syntex@outlook.com>
2022-04-23 15:12:19 +0200

    finish up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,40 +3,80 @@ function App() {
     <div className="h-screen text-lg leading-none bg-fuchsia-50">
       <div className="flex flex-col justify-between p-4 max-w-[375px] h-full">
         <header>
-          <h2 className="text-[128px] font-black leading-[75%]">
-            syra
-            <br />
-            dar
-          </h2>
-          <div>frontend and design</div>
+          <img
+            className="mb-4 text-[128px] font-black leading-[75%]"
+            src="/syradar-logo.png"
+            alt="syradar"
+          />
+
+          <h2>developer and designer</h2>
           <h1 className="font-black">david malmström</h1>
         </header>
 
-        <div className="flex flex-col gap-8">
-          <div
-            className="flex gap-4 p-2 bg-white border border-slate-900"
-            style={{ boxShadow: '8px 8px 0 0 #FF25A4' }}
+        <div>
+          <h2 className="mb-2">skills</h2>
+          <ul className="flex flex-wrap gap-2 justify-between">
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              typescript
+            </li>
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              angular
+            </li>
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              react
+            </li>
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              figma
+            </li>
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              .net
+            </li>
+
+            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
+              agile
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <a
+            href="https://syradar.github.io/yxans-klagan/"
+            className="group grid grid-cols-2 gap-4 p-2 bg-white rounded border border-slate-600"
+            style={{ boxShadow: '4px 4px 0 0 #FF25A4' }}
           >
             <div>
               <div>building</div>
-              <div className="font-black">rpg gm app</div>
+              <div className="font-black group-hover:text-[#FF25A4] group-hover:underline">
+                rpg gm app
+              </div>
             </div>
-            <div>image</div>
-          </div>
+            <img
+              className="grayscale group-hover:grayscale-0 transition"
+              src="/yxans-klagan-logo.png"
+              alt="Rollspelskollektivet"
+            />
+          </a>
 
-          <div
-            className="flex gap-4 p-2 bg-white border border-slate-900"
-            style={{ boxShadow: '8px 8px 0 0 #FF25A4' }}
+          <a
+            href="https://www.rollspelskollektivet.se/"
+            className="group grid grid-cols-2 gap-4 p-2 bg-white rounded border border-slate-600"
+            style={{ boxShadow: '4px 4px 0 0 #FF25A4' }}
           >
             <div>
               <div>layouting</div>
-              <div className="font-black">free rpg products</div>
+              <div className="font-black group-hover:text-[#FF25A4] group-hover:underline">
+                free rpg products
+              </div>
             </div>
-            <div>image</div>
-          </div>
+            <img
+              className="grayscale group-hover:grayscale-0 transition"
+              src="/rk-logo.png"
+              alt="Rollspelskollektivet"
+            />
+          </a>
         </div>
 
-        <div className="flex gap-8 mb-24">
+        <div className="flex gap-8 mb-4">
           <a className="group" href="https://github.com/syradar">
             <div>code</div>
             <div className="font-black text-[#FF25A4] group-hover:underline">
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: 'syradarsite'
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react(), tsconfigPaths()]
+    }
+  } else {
+    return {
+      plugins: [react(), tsconfigPaths()],
+      base: '/syradarsite/'
+    }
+  }
 })
