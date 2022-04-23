6a9554b2e1d92962f060ead608a32ceeb94cc5ed
David Malmström <syntex@outlook.com>
2022-04-23 16:19:24 +0200

    fix a11y and rework skills

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,41 +14,30 @@ function App() {
           <h2>developer and designer in stockholm</h2>
           <h1 className="font-black">david malmström</h1>
         </header>
-
-        <div>
-          <h2 className="mb-2">skills</h2>
-          <ul className="flex flex-wrap gap-2 justify-between">
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              typescript
-            </li>
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              angular
-            </li>
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              react
-            </li>
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              figma
-            </li>
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              .net
-            </li>
-
-            <li className="p-1 text-sm text-slate-600 bg-white rounded border border-slate-600">
-              agile
-            </li>
-          </ul>
-        </div>
-
         <div className="flex flex-col gap-4">
+          <div
+            className="p-2 bg-white rounded border border-slate-600"
+            style={{ boxShadow: '4px 4px 0 0 #d11e87' }}
+          >
+            <h2 className="mb-2">skills</h2>
+            <ul className="flex flex-wrap gap-2 justify-between text-sm text-slate-600">
+              <li>typescript</li>
+              <li>angular</li>
+              <li>react</li>
+              <li>figma</li>
+              <li>.net</li>
+              <li>agile</li>
+            </ul>
+          </div>
+
           <a
             href="https://syradar.github.io/yxans-klagan/"
             className="group grid grid-cols-2 gap-4 p-2 bg-white rounded border border-slate-600"
-            style={{ boxShadow: '4px 4px 0 0 #FF25A4' }}
+            style={{ boxShadow: '4px 4px 0 0 #d11e87' }}
           >
             <div>
               <div>building</div>
-              <div className="font-black group-hover:text-[#FF25A4] group-hover:underline">
+              <div className="font-black text-[#d11e87] group-hover:underline">
                 rpg gm app
               </div>
             </div>
@@ -62,11 +51,11 @@ function App() {
           <a
             href="https://www.rollspelskollektivet.se/"
             className="group grid grid-cols-2 gap-4 p-2 bg-white rounded border border-slate-600"
-            style={{ boxShadow: '4px 4px 0 0 #FF25A4' }}
+            style={{ boxShadow: '4px 4px 0 0 #d11e87' }}
           >
             <div>
               <div>layouting</div>
-              <div className="font-black group-hover:text-[#FF25A4] group-hover:underline">
+              <div className="font-black text-[#d11e87] group-hover:underline">
                 free rpg products
               </div>
             </div>
@@ -81,14 +70,14 @@ function App() {
         <div className="flex gap-8 mb-4">
           <a className="group" href="https://github.com/syradar">
             <div>code</div>
-            <div className="font-black text-[#FF25A4] group-hover:underline">
+            <div className="font-black text-[#d11e87] group-hover:underline">
               github
             </div>
           </a>
 
           <a className="group" href="https://www.linkedin.com/in/hdmalmstrom/">
             <div>work</div>
-            <div className="font-black text-[#FF25A4] group-hover:underline">
+            <div className="font-black text-[#d11e87] group-hover:underline">
               linkedin
             </div>
           </a>
